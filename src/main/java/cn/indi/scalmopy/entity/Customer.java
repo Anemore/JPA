fe777bdfdaf2a978acf847eeb09005978260dd32
Anemore <Contentius1996@163.com>
2024-05-25 13:17:01 +0800

    Hibernate环境搭建

---
+++ src/main/java/cn/indi/scalmopy/entity/Customer.java
@@ -0,0 +1,28 @@
+package cn.indi.scalmopy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "jpa_customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+}
